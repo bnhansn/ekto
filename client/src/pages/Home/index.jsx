2fa4e0f4dd6a38f2b8c70fa0f2c7b2d755b677ca
Ben Hansen <bnhansn@gmail.com>
2016-07-23 12:23:28 -0400

    Testing travis

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -3,8 +3,15 @@ import React, { Component } from 'react';
 class HomePage extends Component {
   render() {
     return (
-      <div className="hero">
-        Home
+      <div className="container">
+        <div className="jumbotron">
+          <div style={{ display: 'flex', alignItems: 'flex-start' }}>
+            <i className="icon icon-cloud2 text-primary" style={{ fontSize: '5rem', marginTop: '9px' }}></i>
+            <h1 style={{ fontSize: '5rem', fontWeight: '400' }}>Billow</h1>
+          </div>
+          <hr />
+          <p className="lead">A blogging platform</p>
+        </div>
       </div>
     );
   }
