2aaa2ec36cda5d31ecc044e76f2f0fd7dc5c8a15
Ben Hansen <bnhansn@gmail.com>
2016-08-17 19:09:26 -0400

    Update markdown guide

--- client/src/components/MarkdownGuide/index.jsx
+++ client/src/components/MarkdownGuide/index.jsx
@@ -11,48 +11,62 @@ const MarkdownGuide = () =>
       </thead>
       <tbody>
         <tr>
-          <td># Heading</td>
+          <td># H1</td>
           <td><h1 className="m-b-0">H1</h1></td>
         </tr>
         <tr>
-          <td>## Heading</td>
+          <td>## H2</td>
           <td><h2 className="m-b-0">H2</h2></td>
         </tr>
         <tr>
-          <td>### Heading</td>
+          <td>### H3</td>
           <td><h3 className="m-b-0">H3</h3></td>
         </tr>
         <tr>
-          <td>**text**</td>
+          <td>**Bold**</td>
           <td><strong>Bold</strong></td>
         </tr>
         <tr>
-          <td>*text*</td>
+          <td>*Italic*</td>
           <td><em>Italic</em></td>
         </tr>
         <tr>
-          <td>~~text~~</td>
+          <td>~~Strike-through~~</td>
           <td><del>Strike-through</del></td>
         </tr>
         <tr>
-          <td>[title](http://)</td>
+          <td>[Link](http://)</td>
           <td><a href="#" onClick={(e) => e.preventDefault()}>Link</a></td>
-        </tr>
-        <tr>
-          <td>`code`</td>
-          <td><code>Inline Code</code></td>
         </tr>
         <tr>
           <td>![alt](http://)</td>
           <td><i className="icon icon-image5 m-r-sm"></i>Image</td>
         </tr>
         <tr>
-          <td>* item</td>
-          <td><li>List</li></td>
+          <td>* List item</td>
+          <td><li>List item</li></td>
         </tr>
         <tr>
-          <td>&gt; quote</td>
+          <td>&gt; Blockquote</td>
           <td><blockquote>Blockquote</blockquote></td>
+        </tr>
+        <tr>
+          <td>`inline code`</td>
+          <td><code>inline code</code></td>
+        </tr>
+        <tr>
+          <td>
+            <span>```</span>
+            <br />
+            <span>multiline code</span>
+            <br />
+            <span>```</span>
+          </td>
+          <td>
+            <pre>
+              <code>multiline code</code>
+            </pre>
+          </td>
         </tr>
         <tr>
           <td>---</td>
